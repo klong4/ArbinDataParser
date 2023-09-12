8fd7f847a89d6ee6cbbfa085f995b3cec61251b0
klong4 <48462651+klong4@users.noreply.github.com>
2023-09-11 23:43:49 -0500

    bigberd

--- server/static/js/main.js
+++ server/static/js/main.js
@@ -4,23 +4,20 @@ document.addEventListener('DOMContentLoaded', () => {
 
 function initPage() {
   const dataWorker = new Worker('static/js/dataWorker.js');
-
-  // Initialize DOM elements
+  
   const partNumberInput = document.getElementById("partNumberInput");
-  const searchResultsDiv = document.getElementById("searchResults"); // For search results
+  const recordList = document.getElementById("recordList");
   const submitButton = document.getElementById('submitButton');
   const searchButton = document.getElementById('searchButton');
   const graphContainer = document.getElementById('graphContainer');
   const backButton = document.getElementById('backButton');
-
-  // Event Listeners
-  attachInputEventListener(partNumberInput, searchResultsDiv);
+  
+  attachInputEventListener(partNumberInput, recordList);
   attachClickEventListener(submitButton, 'submitButton');
   attachClickEventListener(searchButton, 'searchButton');
   attachScrollEventListener(graphContainer, dataWorker);
   attachClickEventListener(backButton, 'backButton');
-
-  // Data Worker Message Handling
+  
   dataWorker.onmessage = handleDataWorkerMessages;
 }
 
@@ -57,20 +54,29 @@ function attachScrollEventListener(containerElement, dataWorker) {
 }
 
 function handleClickEvents(buttonType) {
-  switch(buttonType) {
+  switch (buttonType) {
     case 'submitButton':
-      // Here, you can add the logic for the submit button if needed
+      // Implement submit button logic here
       break;
     case 'searchButton':
       const partNumber = document.getElementById("partNumberInput").value;
-      const searchResultsDiv = document.getElementById("searchResults");
       fetchAndUpdate(`/api/get_records_for_part?part_number=${partNumber}`, data => {
-        searchResultsDiv.innerHTML = '';  // Clear previous results
-
-        if (data.records && data.records.length > 0) {
-          data.records.forEach(record => createRecordLink(record, partNumber, searchResultsDiv));
-        } else {
-          searchResultsDiv.innerHTML = 'No records found';
+        const searchResultsDiv = document.getElementById("searchResults");
+        if (searchResultsDiv) {
+          searchResultsDiv.innerHTML = ''; // Clear previous results
+          const seenCombinations = new Set(); // Track unique date-part combos
+          if (data.records && data.records.length > 0) {
+            data.records.forEach(record => {
+              const date = record.test_time; // Replace with the actual date property
+              const uniqueKey = `${date}-${partNumber}`;
+              if (!seenCombinations.has(uniqueKey)) {
+                createRecordLink(record, partNumber, searchResultsDiv);
+                seenCombinations.add(uniqueKey);
+              }
+            });
+          } else {
+            searchResultsDiv.innerHTML = 'No records found';
+          }
         }
       });
       break;
@@ -84,30 +90,22 @@ function fetchAndUpdate(url, callback, options = {}) {
   fetch(url, options)
     .then(response => response.json())
     .then(data => {
-      console.log("Debug: Data Type:", typeof data);
-      console.log("Debug: Full Data Object:", data);
-
-      const debugMessage = `Debug: Received Data Length ${data?.length || 'N/A'}, Type: ${typeof data}`;
-      fetch('/api/debug', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ message: debugMessage })
-      });
-
+      console.log('Data from server:', data);  // Debug line
       callback(data);
     })
     .catch(error => console.error('Error:', error));
 }
 
 function createRecordLink(record, partNumber, parentElement) {
-  if (!record.test_time) { // Changed from test_date to test_time
+  if (!record.test_time) { 
     console.log(`Skipping record due to missing test_time: ${JSON.stringify(record)}`);
     return;
   }
+  const formattedDate = record.test_time.split('/').join('-'); // Replace forward slashes with dashes
   const listItem = document.createElement("a");
-  listItem.textContent = `${record.test_time} - ${record.other_identifier}`; // Changed from test_date to test_time
-  listItem.href = `/show_graph/${partNumber}/${record.test_time}`; // Changed from test_date to test_time
-  listItem.className = "record-link";
+  listItem.textContent = `${record.test_time}`; 
+  listItem.href = `/show_graph/${partNumber}/${formattedDate}`; // Use formatted date
+  listItem.className = "record-button";
   parentElement.appendChild(listItem);
 }
 
@@ -123,4 +121,17 @@ function handleDataWorkerMessages(e) {
   } else if (type === 'LAZY_LOAD') {
     console.log("Received lazy-load data:", payload);
   }
+}
+
+function createRecordLink(record, partNumber, parentElement) {
+  if (!record.test_time) { 
+    console.log(`Skipping record due to missing test_time: ${JSON.stringify(record)}`);
+    return;
+  }
+  const formattedDate = record.test_time.split('/').join('-'); // Replace forward slashes with dashes
+  const listItem = document.createElement("a");
+  listItem.textContent = `${record.test_time}`; 
+  listItem.href = `/show_graph/${partNumber}/${formattedDate}`; // Use formatted date
+  listItem.className = "record-button";  // Updated class name
+  parentElement.appendChild(listItem);
 }
